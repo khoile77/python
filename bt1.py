aee999e3860791364a4f2b45038e4da730573ee1
khoile77 <154046980+khoile77@users.noreply.github.com>
2024-08-17 21:00:20 +0700

    Add files via upload

---
+++ bt1.py
@@ -0,0 +1,134 @@
+#bai1
+#name = str(input("What your name?"))
+#print("Welcome,"+name)
+
+#bai2
+#First_name = str(input("What your first name?"))
+#Last_name = str(input("What your last name?"))
+#Phone_number = input("What your phone number?")
+
+#print("Your registered name"+" "+First_name+Last_name)
+#print("Your phone number is" +" "+Phone_number)
+
+#bai3
+name = input("Input your name: ")
+birthday_date = input("Input your birthdate??: ")
+birthday_month = input("Input your birth month??: ")
+birthday_year = input("Input your birth year??: ")
+print(f"Name\t\t:\t\t{name}")
+print(f"Birthday\t:\t\t{birthday_date}/{birthday_month}/{birthday_year}")
+
+# # Bài 4:
+#year = input("What year is it? ")
+#print(f"""
+#      
+
+#                           . :  . 
+#                         '.   :   .'
+#HAPPY NEW YEARS      .   '. : .'   .
+#   !!! {year} !!!        .    .'  :'.   .
+#                         .'  :  '  .
+#                           ' : '
+# """)
+# #Bài 5:
+#import turtle
+#t = turtle.Turtle()
+
+#t.forward(50)
+#t.left(120)
+#t.forward(100)
+#t.left(120)
+#t.forward(100)
+#t.left(120)
+#t.forward(50)
+
+#turtle.done()
+# Bài 6:
+# import turtle
+# t = turtle.Turtle()
+
+# t.left(90)
+# t.penup()
+# t.forward(100)
+# t.right(90)
+# t.pendown()
+# t.forward(100)
+# t.right(90)
+# t.forward(200)
+# t.right(90)
+# t.forward(200)
+# t.right(90)
+# t.forward(200)
+# t.right(90)
+# t.forward(100)
+# t.left(90)
+# t.penup()
+# t.forward(20)
+# t.right(90)
+# t.pendown()
+# t.pensize(2)
+# t.forward(120)
+# t.right(90)
+# t.forward(240)
+# t.right(90)
+# t.forward(240)
+# t.right(90)
+# t.forward(240)
+# t.right(90)
+# t.forward(120)
+
+
+
+
+# turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Bài 7:
+# import turtle
+# k = turtle.Turtle()
+
+# k.left(90)
+# k.penup()
+# k.forward(100)
+# k.right(90)
+# k.pendown()
+# k.forward(100)
+# k.right(90)
+# k.forward(200)
+# k.right(90)
+# k.forward(200)
+# k.right(90)
+# k.forward(200)
+# k.right(90)
+# k.forward(100)
+# k.left(90)
+# k.penup()
+# k.forward(40)
+# k.pendown()
+# k.right(135)
+# k.forward(200)
+# k.right(90)
+# k.forward(200)
+
+# k.right(90)
+# k.forward(200)
+# k.right(90)
+# k.forward(200)
+
+# turtle.done()

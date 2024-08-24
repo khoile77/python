89e90a5192290a4a3da8c53df0a3b076bd0befe4
khoile77 <154046980+khoile77@users.noreply.github.com>
2024-08-24 20:39:55 +0700

    Add files via upload

---
+++ bt2.py
@@ -0,0 +1,29 @@
+###baitap2
+#1 A
+#2 C
+#3 C
+#4 A
+#5 A
+#6 A
+#7 A
+#8 A
+#9 D
+#10 A
+
+
+
+# danh_sanh = input("Nhập các giá trị, cách nhau bằng dấu phẩy: ")
+# nums = danh_sanh1.split(",")
+# nums = [int(num) for num in nums]
+
+
+
+
+# def tinh_trung_binh(nums):
+#     return sum(nums) / len(nums)
+
+
+# mean = tinh_trung_binh(nums)
+# print("Giá trị trung bình là:", mean)
+
+
